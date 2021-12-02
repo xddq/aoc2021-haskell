b32a444763098f3e4a9e9d04fc97ebb3b456117d
git@pierrefhapp.dev <git@pierrefhapp.dev>
2021-12-02 12:20:25 +0100

    9min. cleans up the code, adds comments.

--- Day2.hs
+++ Day2.hs
@@ -7,26 +7,25 @@
 -- forward 2
 main = do
   input <- readFile "inputDay2"
-  putStrLn $
-    show $
-    ex1 (0, 0) $
-    map
-      (\line ->
-         let (command:value) = words line
-          in (command, toInt $ head value)) $
-    lines input
-  putStrLn $
-    show $
-    ex2 (0, 0, 0) $
-    map
-      (\line ->
-         let (command:value) = words line
-          in (command, toInt $ head value)) $
-    lines input
+  let preparedInput = prepareInput input
+  print $ ex1 (0, 0) preparedInput
+  print $ ex2 (0, 0, 0) preparedInput
   return ()
+
+-- Creates a list of string value tuples. [(forward, 5), (down, 5), etc...]
+prepareInput :: String -> [(String, Int)]
+prepareInput input =
+  map
+    (\line ->
+       let (command:value) = words line
+        in (command, toInt $ head value)) $
+  lines input
   where
     toInt xs = read xs :: Int
 
+-- Adapts the current X and Y positions of the submarine depending on the given
+-- command. Multiplies resulting X and Y positions once we iterated through all
+-- commands.
 ex1 :: (Int, Int) -> [(String, Int)] -> Int
 ex1 (xPos, yPos) [] = xPos * yPos
 ex1 (xPos, yPos) ((command, value):xs)
@@ -34,6 +33,8 @@ ex1 (xPos, yPos) ((command, value):xs)
   | command == "up" = ex1 (xPos, yPos - value) xs
   | otherwise = ex1 (xPos, yPos + value) xs
 
+-- Adapted semantic of up, down and foward based on description. Added aim as
+-- thrid parameter to keep track of.
 ex2 :: (Int, Int, Int) -> [(String, Int)] -> Int
 ex2 (xPos, yPos, aim) [] = xPos * yPos
 ex2 (xPos, yPos, aim) ((command, value):xs)
