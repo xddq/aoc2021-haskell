e7383b5c09fef73af7c6f7f90e7fdbd1bb77c7d0
git@pierrefhapp.dev <git@pierrefhapp.dev>
2021-12-19 20:45:15 +0100

    somehow get a to large number. figure out mistake for pt 2

--- Day12.hs
+++ Day12.hs
@@ -12,9 +12,9 @@ import Data.Char
 main
   -- input <- lines <$> readFile "inputDay11"
  = do
-  input <- lines <$> readFile "inputDay12"
-  print $ ex1 input
-  -- print $ ex2 input
+  input <- lines <$> readFile "testInput"
+  -- print $ ex1 input
+  print $ ex2 input
   return ()
 
 type Node = [Char]
@@ -30,21 +30,21 @@ ex1 input =
   length $
   map (tail . reverse) $
   filter (not . null) $ walk "start" [[]] $ mkEdgeMap input
-  where
-    mkEdgeMap :: [[Char]] -> EdgeMap
-    mkEdgeMap input
-          -- parses input which looks like ["x-y",..] to "["x","y"],..] where x and
-          -- y will be an edge with x being the start and y the end node.
-     =
-      let edges = map (splitOn "-") input
-            -- Creates bidirectional Map of edges consisting of the individual
-            -- nodes as keys and all the nodes they are connected with as
-            -- values.
-       in M.fromListWith (++) $
-          concat $
-          [ [(head edge, tail edge), (head $ tail edge, [head edge])]
-          | edge <- edges
-          ]
+
+mkEdgeMap :: [[Char]] -> EdgeMap
+mkEdgeMap input
+      -- parses input which looks like ["x-y",..] to "["x","y"],..] where x and
+      -- y will be an edge with x being the start and y the end node.
+ =
+  let edges = map (splitOn "-") input
+        -- Creates bidirectional Map of edges consisting of the individual
+        -- nodes as keys and all the nodes they are connected with as
+        -- values.
+   in M.fromListWith (++) $
+      concat $
+      [ [(head edge, tail edge), (head $ tail edge, [head edge])]
+      | edge <- edges
+      ]
 
 -- ex1 input = mkEdgeMap input
 endNode = "end"
@@ -74,8 +74,45 @@ useNode node path
   | all isLower node && node `elem` path = False
   | otherwise = True
 
--- edgeMap :: [[Char]] -> Map [Char] [[Char]]
--- edgeMap input = foldl' (\edgeMap (start,end) -> ) M.empty
--- addEdge :: Map [Char] [[Char]] -> [Char]
-ex2 :: [[Char]] -> Int
-ex2 input = 2
+-- ex2 :: [[Char]] -> Int
+ex2 input
+  -- length $
+  -- nub $
+ =
+  length $
+  map (tail . reverse) $
+  filter (not . null) $ walk2 startNode [[]] $ mkEdgeMap input
+
+walk2 :: Node -> Path -> Map Node [Node] -> [Path]
+walk2 node visited edgeMap =
+  if node == endNode
+    then [node : visited]
+    else case useNode2 node visited of
+           True ->
+             case M.lookup node edgeMap of
+               Just stepableNodes ->
+                 concatMap
+                   (\nextNode -> walk2 nextNode (node : visited) edgeMap)
+                   stepableNodes
+               Nothing -> [emptyPath]
+           otherwise -> [emptyPath]
+
+-- Determines whether we can use this node for walking towards the goal.
+useNode2 :: Node -> Path -> Bool
+useNode2 node path
+  | node `elem` [startNode, endNode] && (not $ node `elem` path) = True
+  | and [all isLower node, visitedTwice path] = False
+  | otherwise = True
+
+canBeVisitedTwice :: Node -> Bool
+canBeVisitedTwice node
+  | node `elem` [startNode, endNode] = False
+  | otherwise = True
+
+-- MAYBE(pierre): this has bad runtime. Could adapt arguments of useNode2 and
+-- walk2 and pass a flag to fix this after solution is done.
+visitedTwice :: Path -> Bool
+visitedTwice [] = False
+visitedTwice (x:xs)
+  | all isLower x && x `elem` xs = True
+  | otherwise = visitedTwice xs
